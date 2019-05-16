8c3794199e2e996bce4a07d5aa0b3445b160af69
Evan Dadure <evan.dadure@gmail.com>
2019-05-16 09:35:00 +0200

    commit day/week

--- src/Day.py
+++ src/Day.py
@@ -2,4 +2,10 @@ class Day():
 
     def __init__(self, day_name):
         self.day_name = day_name
+        # a timetable is composed of 12 lists representing the 12 timestamps "00-02", "02-04"..."22-00"
         self.timetable = [[],[],[],[],[],[],[],[],[],[],[],[]]
+
+    def fillDay(self, tweet):
+        return 0
+
+
---
+++ src/Week.py
@@ -0,0 +1,26 @@
+from Day import Day
+
+class TweetsList():
+
+    def __init__(self):
+        self.monday = Day("Monday")
+        self.tuesday = Day("Tuesday")
+        self.wednesday = Day("Wednesday")
+        self.thursday = Day("Thursday")
+        self.friday = Day("Friday")
+        self.saturday = Day("Saturday")
+        self.sunday = Day("Sunday")
+
+
+
+    def fillWeek(self, list_tweets):
+        # parcours la liste de tweets et ajoute dans le jour correspondant à la date du tweet
+        # l'id du follower du tweet, au bon horodatage
+        return 0
+
+
+
+
+
+
+
